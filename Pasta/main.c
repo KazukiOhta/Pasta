aa66d480e57ab89663506989ccc30c7d3feb874f
太田　一毅 <kazukiota121@gmail.com>
2015-09-14 23:02:17 +0900

    Initial Commit

---
+++ Pasta/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pasta
+//
+//  Created by 太田　一毅 on 2015/09/14.
+//  Copyright (c) 2015年 太田一毅. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
